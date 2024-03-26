3c0aa299ac78c460db4320f62edcf0a301fe4e8d
TicklishReboots <67234652+TicklishReboots@users.noreply.github.com>
2024-03-26 20:45:17 +0100

    initial commit

---
+++ App.tsx
@@ -0,0 +1,67 @@
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import Constants from "expo-constants";
+import * as SecureStore from "expo-secure-store";
+import * as Location from "expo-location";
+import { useAuth } from "@clerk/clerk-expo";
+
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
+const SignOut = () => {
+  const { isLoaded, signOut } = useAuth();
+  if (!isLoaded) {
+    return null;
+  }
+  return (
+    <View>
+      <Button
+        title="Sign Out"
+        onPress={() => {
+          signOut();
+        }}
+      />
+    </View>
+  );
+};
+
+export default function App() {
+  return (
+    <ClerkProvider
+      publishableKey={Constants.expoConfig.extra.clerkPublishableKey}
+      tokenCache={tokenCache}
+    >
+      <SafeAreaProvider>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!!!!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </SafeAreaProvider>
+    </ClerkProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ app.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "moviemindr",
+  version: "0.0.1",
+  extra: {
+    clerkPublishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+  },
+};
